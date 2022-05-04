54dd3182faddc0f202ffc7c46a854f7a0dec2f91
Filipe Dias <filipediascontato@gmail.com>
2022-05-04 16:08:14 -0300

    Finishes the FeedbackContentStep form style

--- web/src/components/FeedbackContentStep/index.tsx
+++ web/src/components/FeedbackContentStep/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { ArrowLeft } from "phosphor-react";
+import { ArrowLeft, Camera } from "phosphor-react";
 
 import { CloseButton } from "../CloseButton";
 import { FeedbackType, feedbackTypes } from "../Form";
@@ -43,6 +43,22 @@ export const FeedbackContentStep: React.FC<Props> = ({
           placeholder={feedbackTypeData.placeholder}
           className="min-w-[314px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md scrollbar-thin scrollbar-thumb-zinc-700 scrollbar-track-transparent focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none"
         />
+
+        <footer className="flex gap-2 mt-2">
+          <button
+            type="button"
+            className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-brand-500 focus:ring-offset-2 focus:ring-offset-zinc-900 transition-colors"
+          >
+            <Camera className="w-6 h-6" />
+          </button>
+
+          <button
+            type="submit"
+            className="p-2 bg-brand-500 rounded-md border-transparent flex flex-1 items-center justify-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-brand-500 focus:ring-offset-2 focus:ring-offset-zinc-900 transition-colors"
+          >
+            Send Feedback
+          </button>
+        </footer>
       </form>
     </>
   );
