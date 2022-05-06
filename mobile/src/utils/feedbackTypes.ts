52bd06bd38e5cb65b80500151cf92bbb6f17075c
Filipe Dias <filipediascontato@gmail.com>
2022-05-06 15:50:50 -0300

    Creates the mobile repository

---
+++ mobile/src/utils/feedbackTypes.ts
@@ -0,0 +1,15 @@
+export const feedbackTypes = {
+  'bug': {
+    title: 'Bug',
+    image: require('../assets/bug.png')
+  },
+  'idea': {
+    title: 'Idea',
+    image: require('../assets/idea.png'),
+  },
+  'thought': {
+    title: 'Thought',
+    image: require('../assets/thought.png'),
+  },
+};
+
