36913f76dbb3ec1951e151c1986c92fc5d9c8167
Filipe Dias <filipediascontato@gmail.com>
2022-05-08 00:27:25 -0300

    Implements options behaviour

--- mobile/src/components/Options/index.tsx
+++ mobile/src/components/Options/index.tsx
@@ -4,16 +4,26 @@ import { View, Text } from "react-native";
 import { feedbackTypes } from "../../utils/feedbackTypes";
 import { Copyright } from "../Copyright";
 import { Option } from "../Option";
+import { FeedbackType } from "../Widget";
 import { styles } from "./styles";
 
-export const Options: React.FC = () => {
+interface Props {
+  onFeedbackTypeChanged: (feedbackType: FeedbackType) => void;
+}
+
+export const Options: React.FC<Props> = ({ onFeedbackTypeChanged }: Props) => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Give your feedback</Text>
 
       <View style={styles.options}>
         {Object.entries(feedbackTypes).map(([key, value]) => (
-          <Option key={key} title={value.title} image={value.image} />
+          <Option
+            key={key}
+            title={value.title}
+            image={value.image}
+            onPress={() => onFeedbackTypeChanged(key as FeedbackType)}
+          />
         ))}
       </View>
 
