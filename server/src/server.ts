856de3de83d9667ec4a394d613d260fceb3bed67
Filipe Dias <filipediascontato@gmail.com>
2022-05-08 23:23:30 -0300

    Updates server log

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,5 +10,5 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(process.env.PORT || 3333, () => {
-  console.log("Server running on http://localhost:3333");
+  console.log("Server running!");
 });
